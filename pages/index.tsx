e0453986e4e1126aa6ca2e02fde998d30456c4fe
Luca Caputo <caputo.luca@outlook.com>
2021-02-27 11:00:47 +0100

    added gitignore

---
+++ pages/index.tsx
@@ -0,0 +1,12 @@
+import Head from 'next/head'
+import { NextPage } from "next"
+
+const Index: NextPage = () => {
+    return (
+        <div className="home">
+            Home
+        </div>
+    );
+}
+
+export default Index;
