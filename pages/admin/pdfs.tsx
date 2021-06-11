e33f8e56c2195526ff6ed6469a82aedb4d7f3c23
Luca Caputo <caputo.luca@outlook.com>
2021-06-11 15:22:21 +0200

    added initial upload pdf logic

--- pages/admin/pdfs.tsx
+++ pages/admin/pdfs.tsx
@@ -1,13 +1,36 @@
 import { NextPage } from "next";
 import FilePicker from "../../components/admin/FilePicker";
+import { API_BASE } from '../../helpers/index';
+import useSWR from 'swr';
 
 const Pdfs: NextPage = () => {
-    
+    const upload = async (files: File[]) => {
+        const body = new FormData();
+        files.forEach(f => body.append('pdfs[]', f));
+        await fetch(`${API_BASE}/projects/pdfs`, {
+            method: 'POST',
+            headers: {
+                'Authorization': `Bearer ${window.localStorage.getItem('token')}`,
+            },
+            body,
+        })
+        .then(res => res.text())
+        .then(data => {
+            console.log('data', data);
+            mutate();
+        })
+        .catch(err => {
+            console.log('error', err);
+        })
+    }
+    const { data, error, mutate } = useSWR(`${API_BASE}/projects/pdfs`, (inp: RequestInfo, init: RequestInit) => fetch(inp, init).then(r => r.json()));
+    console.log('data from swr', data);
+    console.log('error from swr', error);
     return (
         <>
             <div className="container mainContainer" style={{ perspective: 200 }}>
                 <FilePicker 
-                    onUpload={f => null} 
+                    onUpload={upload} 
                     allowedExtensions={['application/pdf']} 
                     title="Carica i PDF"
                     subtitle="Oppure trascinali nello scatolo ostia"
