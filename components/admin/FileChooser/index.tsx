fe04fb088b07658b642677c77da4b291d98a9777
Luca Caputo <caputo.luca@outlook.com>
2021-12-21 14:01:12 +0100

    merge 2

--- components/admin/FileChooser/index.tsx
+++ components/admin/FileChooser/index.tsx
@@ -1,56 +1,3 @@
-<<<<<<< HEAD
-import { motion } from "framer-motion";
-import { useState } from "react";
-import useSWR from "swr";
-import { API_BASE } from "../../../helpers";
-
-type FileChooserProps = {
-    selectedAction: (urls: string[]) => void,
-    staticFetchPath: string,
-}
-
-type ImagesApiResponse = {
-    ok: boolean,
-    images: {
-        md5: string,
-        name: string,
-        url: string,
-        __v: number,
-        _id: string,
-    }[],
-}
-
-const FileChooser: React.FC<FileChooserProps> = ({ selectedAction, staticFetchPath }) => {
-    const { data, mutate, isValidating, error } = useSWR<ImagesApiResponse, any>(API_BASE + staticFetchPath);
-    const loading = (!data && !error) || isValidating;
-    console.log(data);
-    if (data) data.images = !loading ? ([...Array(120).keys()].map(_ => data.images[0])) : [];
-    return (
-        <>
-            <motion.div
-                className="chooser_wrapper"
-                style={{ paddingLeft: 75, display: 'flex', width: '100%', flexWrap: 'wrap' }}
-            >
-                {
-                    !loading &&
-                    data.images.map(i => (
-                        <motion.div 
-                            key={i.name}
-                            style={{
-                                width: 50,
-                                height: 50,
-                                background: `url(${API_BASE}/${i.url}) no-repeat`,
-                                backgroundSize: 'cover',
-                                backgroundPosition: 'center',
-                            }}
-                            className="m-2"
-                        >
-
-                        </motion.div>
-                    ))
-                }
-            </motion.div>
-=======
 import { useState, useEffect, CSSProperties } from "react";
 import FileChooserEntry from './FileChooserEntry';
 import { AnimatePresence, motion, Variants } from "framer-motion";
@@ -190,14 +137,8 @@ function FileChooser<T extends BaseFile>(props: FileChooserProps<T>): React.Reac
                     </AnimatePresence>
                 }
             </div>
->>>>>>> develop
         </>
     );
 }
 
-<<<<<<< HEAD
 export default FileChooser;
-
-=======
-export default FileChooser;
->>>>>>> develop
