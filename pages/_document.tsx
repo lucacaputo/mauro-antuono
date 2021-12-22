3e22157ce042d3acf3b9e2d96a87c2c858607fdf
Luca Caputo <caputo.luca@outlook.com>
2021-12-22 17:51:06 +0100

    gtag

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -7,7 +7,7 @@ export default class Doc extends Document {
                 <Head>
                     <script 
                         async
-                        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`} 
+                        src={`https://www.googletagmanager.com/gtag/js?id=G-FWBRH5QL09`} 
                     />
                     <script 
                         dangerouslySetInnerHTML={{
@@ -16,7 +16,7 @@ export default class Doc extends Document {
                                 function gtag(){dataLayer.push(arguments);}
                                 gtag('js', new Date());
                             
-                                gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+                                gtag('config', 'G-FWBRH5QL09', {
                                     page_path: window.location.pathname
                                 });
                             `
